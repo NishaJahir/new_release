0c6ca856b965f44ae1e48929d7a2ca2953bbf327
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2020-03-30 16:20:46 +0530

    Update TransactionService.php

--- src/Services/TransactionService.php
+++ src/Services/TransactionService.php
@@ -29,7 +29,23 @@ use Novalnet\Services\PaymentService;
 class TransactionService
 {
     use Loggable;
-     
+    
+    /**
+	 *
+	 * @var PaymentService
+	 */
+	private $paymentService;
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param PaymentService $paymentService
+	 */
+	 
+    public function __construct(PaymentService $paymentService)
+    {
+	    $this->paymentService  = $paymentService;
+	}
 	
 	
 	
@@ -78,16 +94,25 @@ class TransactionService
     {
         $database = pluginApp(DataBase::class);
         $order    = $database->query(TransactionLog::class)->where($key, '=', $value)->get();
+        $db_details = $this->paymentService->getDatabaseValues($value);
+        $this->getLogger(__METHOD__)->error('db', $db_details);
         $update_info = $order[0];
         $additional_info = json_decode($update_info->additionalInfo, true);
         $update_additional_info = [
-            'due_date' => '2020-05-01'
+        'invoice_bankname'  => !empty($invoiceDetails['invoice_bankname']) ? $invoiceDetails['invoice_bankname'] : $db_details['invoice_bankname'],
+	    'invoice_bankplace' => !empty($invoiceDetails['invoice_bankplace']) ? $invoiceDetails['invoice_bankplace'] : $db_details['invoice_bankplace'],
+	    'invoice_iban'      => !empty($invoiceDetails['invoice_iban']) ? $invoiceDetails['invoice_iban'] : $db_details['invoice_iban'],
+	    'invoice_bic'       => !empty($invoiceDetails['invoice_bic']) ? $invoiceDetails['invoice_bic'] : $db_details['invoice_bic'],
+	    'due_date'          => !empty($invoiceDetails['due_date']) ? $invoiceDetails['due_date'] : $db_details['due_date'],
+	    'invoice_type'      => !empty($invoiceDetails['invoice_type']) ? $invoiceDetails['invoice_type'] : $db_details['invoice_type'] ,
+	    'invoice_account_holder' => !empty($invoiceDetails['invoice_account_holder']) ? $invoiceDetails['invoice_account_holder'] : $db_details['invoice_account_holder']    
+            
         ];
         $additional_info = array_merge($additional_info, $update_additional_info);
         $update_info->additionalInfo = json_encode($additional_info);
-        
+        $this->getLogger(__METHOD__)->error('info', $update_info);
         $database->save($update_info);
-	$this->getLogger(__METHOD__)->error('todo',$update_info);
+
         return $update_info;
     }
 	
